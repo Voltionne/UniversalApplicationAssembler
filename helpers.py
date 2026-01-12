919758c7451b3e2a4e05cbe9111ad7269232edae
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2026-01-12 20:56:05 +0100

    Added gradient method

--- helpers.py
+++ helpers.py
@@ -1,3 +1,16 @@
+def gradient_range(a: int, b: int):
+
+    """
+    Creates an iterator that returns all values from a to b, creating a gradient. For example, if a = 3 and b = 5, it returns 3, 4 and 5. If a = 2 and b = -1, it returns 2, 1, 0, -1
+    
+    :param a: Initial value
+    :type a: int
+    :param b: Final value
+    :type b: int
+    """
+
+    return range(a, b + (1 if a < b else -1), 1 if a < b else -1)
+
 def is_number_colon_number(s: str) -> bool:
 
     """
