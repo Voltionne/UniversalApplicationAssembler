f31ef2a5a071298029200990601a073ef9b33dbc
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2026-02-14 15:03:25 +0100

    Added example test of BitRange

--- src/UniversalApplicationCompiler/Main.scala
+++ src/UniversalApplicationCompiler/Main.scala
@@ -4,4 +4,9 @@ import UniversalApplicationCompiler.helpers.BitRange
 
 object Main:
   def main(args: Array[String]): Unit =
-    println("Runs!")
+    val bitRange = BitRange(5, 0)
+    println(bitRange.value)
+    println(bitRange.checkValue)
+    bitRange.setFullValue(15)
+    println(bitRange.value)
+    println(bitRange.checkValue)
