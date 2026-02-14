530f52c822d9323ff3d95d7b04ba348bfd381863
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2026-02-14 17:32:40 +0100

    Changed to temporal code

--- src/UniversalApplicationCompiler/Main.scala
+++ src/UniversalApplicationCompiler/Main.scala
@@ -4,9 +4,4 @@ import UniversalApplicationCompiler.helpers.BitRange
 
 object Main:
   def main(args: Array[String]): Unit =
-    val bitRange = BitRange(5, 0)
-    println(bitRange.value)
-    println(bitRange.checkValue)
-    bitRange.setFullValue(15)
-    println(bitRange.value)
-    println(bitRange.checkValue)
+    println("UniversalApplicationCompiler v0.2")
