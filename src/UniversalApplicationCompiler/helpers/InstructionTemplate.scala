4e39a95dca015761c6c26639df353d3e1834d31c
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2026-02-14 17:31:52 +0100

    Added assertion

--- src/UniversalApplicationCompiler/helpers/InstructionTemplate.scala
+++ src/UniversalApplicationCompiler/helpers/InstructionTemplate.scala
@@ -1,4 +1,18 @@
 package UniversalApplicationCompiler.helpers
 
-case class InstructionTemplate():
-  ???
+import UniversalApplicationCompiler.helpers.Functions.gradientRange
+
+case class InstructionTemplate(bits: Int, fields: Map[String, BitRange]):
+
+  require: //Check that no bit collisions
+    val usedBits: Array[Boolean] = Array.fill(bits)(false)
+
+    //Hating Scala because no imperative nice for loop here
+    fields.forall { field =>
+      gradientRange(field(1).a, field(1).b).forall { idx =>
+        if usedBits(idx) then false
+        else {usedBits(idx) = true; true}
+      }
+    }
+
+  def apply(): Unit = ???
