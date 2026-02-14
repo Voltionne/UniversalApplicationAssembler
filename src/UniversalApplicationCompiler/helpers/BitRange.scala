fbce02567d83ddf2aebe4c2f8e8ada1c10cf32a1
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2026-02-14 14:52:44 +0100

    Implemented setPartialValue

--- src/UniversalApplicationCompiler/helpers/BitRange.scala
+++ src/UniversalApplicationCompiler/helpers/BitRange.scala
@@ -1,11 +1,53 @@
 package UniversalApplicationCompiler.helpers
 
 import scala.math._
+import UniversalApplicationCompiler.helpers.Functions.gradientRange
 
 case class BitRange(a: Int, b: Int):
   val bits: Int = abs(a - b) + 1
-  val value: String = "0" * bits
+  var value: String = "0" * bits
+  val endianness: String = if a > b then "little" else "big"
 
-  def setPartialValue(setDict: Map[String, Any]): Unit = ???
+  def setPartialValue(setDict: Map[String, Any]): Unit =
+    require(setDict contains "set")
+    require(setDict("set").isInstanceOf[Int])
+
+    require(setDict contains "bits")
+    require(setDict("bits").isInstanceOf[String])
+
+    (setDict("set"), setDict("bits")) match
+      case (set: Int, bits: String) =>
+        if bits contains ":" then
+
+          //This means it specifies a whole range
+          val parts = bits.split(":")
+
+          require(parts.length == 2)
+          require(parts(0).forall(p => p.isDigit) && parts(1).forall(p => p.isDigit)) //check that both numbers are numeric
+
+          val bitsSet = abs(parts(0).toInt - parts(1).toInt) + 1
+
+          require(set >= 0 && set < pow(2, bitsSet))
+
+          val setValueBinArray: Array[Char] = (f"${set.toBinaryString}%8s".replace(' ', '0')).toCharArray
+
+          val valueAsArrayReverse: Array[Char] = value.toCharArray.reverse //convert to array temporally (because string is immutable)
+
+          for (i, idx) <- gradientRange(parts(0).toInt, parts(1).toInt).zipWithIndex do
+            valueAsArrayReverse(i) = setValueBinArray(idx)
+
+          value = valueAsArrayReverse.reverse.mkString
+
+        else
+
+          require(bits.forall(p => p.isDigit)) //check that bits is a digit
+          require(set == 0 || set == 1) //check that set is a binary digit
+
+          val valueAsArrayReverse: Array[Char] = value.toCharArray.reverse
+
+          valueAsArrayReverse(bits.toInt) = set.toChar
+
+          value = valueAsArrayReverse.reverse.mkString
+
   def setFullValue(value: Int): Unit = ???
   def checkValue: Boolean = ???
