ebf635dacf704f34abfad10c63aa35506a98e140
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2026-02-16 14:44:40 +0100

    Renamed FromJava to Conversions and added convertParametersMap

--- src/UniversalApplicationCompiler/IsaParser.scala
+++ src/UniversalApplicationCompiler/IsaParser.scala
@@ -2,13 +2,13 @@ package UniversalApplicationCompiler
 
 import org.yaml.snakeyaml.Yaml
 import java.nio.file.{Files, Paths}
-import UniversalApplicationCompiler.helpers.FromJava
+import UniversalApplicationCompiler.helpers.Conversions
 
 class IsaParser(val yamlConfigPath: String, var autoParse: Boolean = true):
   val yamlData: Any =
     val yaml = new Yaml()
     val raw = yaml.load(Files.newInputStream(Paths.get(yamlConfigPath)))
-    FromJava.convert(raw)
+    Conversions.convertFromJava(raw)
 
   //Auto-parse during construction if autoParse is enabled.
   if autoParse then parse()
---
+++ src/UniversalApplicationCompiler/helpers/Conversions.scala
@@ -0,0 +1,61 @@
+package UniversalApplicationCompiler.helpers
+
+import scala.collection.mutable
+import scala.jdk.CollectionConverters.*
+
+/**
+ * Represents the parameters of a function
+ * @param values the datatypes of each of the parameters
+ * @param mappings the fields where each parameter maps.
+ */
+case class ParametersDefinition(values: List[String], mappings: List[String | List[String]]):
+  require(values.length == mappings.length)
+
+
+/**
+ * Convert things from Java to Scala (as Scala uses the JDK and some libraries use datatypes of Java)
+ */
+object Conversions:
+
+  /**
+   * Automatically converts a YAML file maps, sets, and lists from Java to Scala types.
+   * @param value the value to be parsed
+   * @return the converted type
+   */
+  def convertFromJava(value: Any): Any = value match
+    
+    case m: java.util.Map[?, ?] =>
+      mutable.LinkedHashMap.from(m.asScala.view.map { case (k, y) => convertFromJava(k) -> convertFromJava(y) })
+    case l: java.util.List[?] =>
+      l.asScala.view.map(convertFromJava).toList
+    case s: java.util.Set[?] =>
+      s.asScala.view.map(convertFromJava).toSet
+    case other => other
+
+  /**
+   * Converts a map that represents the parameters of instructions to the case class ParameterDefinition
+   * @param map the map that represents the parameters of an instruction
+   * @return ParametersDefinition class instance
+   */
+  def convertParametersMap(map: Map[String, Any]): ParametersDefinition =
+
+    val values: List[String] = map.get("Values") match
+      case Some(l: List[_]) =>
+        if l.forall(_.isInstanceOf[String]) then l.asInstanceOf[List[String]]
+        else throw new IllegalArgumentException("values must be a list of Strings")
+      case Some(_) => throw new IllegalArgumentException("values must be a list")
+      case None => throw new IllegalArgumentException("missing 'values'")
+    
+    val mappings: List[String | List[String]] = map.get("mappings") match
+      case Some(l: List[_]) =>
+        if l.forall {
+          case _: String => true
+          case l: List[_] => l.forall(_.isInstanceOf[String])
+          case _ => false
+        } then l.asInstanceOf[List[String | List[String]]]
+        else throw new IllegalArgumentException("mappings must be a list that contains either Strings or lists of Strings")
+      case Some(_) => throw new IllegalArgumentException("mappings must be a list")
+      case None => throw new IllegalArgumentException("missing 'mappings'")
+
+    ParametersDefinition(values, mappings)
+
