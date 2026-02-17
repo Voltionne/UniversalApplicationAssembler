9ddde11a372da00ba820d2efd5cf7d88353e6e31
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2026-02-17 19:38:47 +0100

    New ParameterMappings!

--- src/UniversalApplicationCompiler/helpers/ParametersDefinition.scala
+++ src/UniversalApplicationCompiler/helpers/ParametersDefinition.scala
@@ -1,11 +1,15 @@
 package UniversalApplicationCompiler.helpers
 
+sealed trait ParameterMapping
+case class SingleParameterMapping(mappingLocation: String) extends ParameterMapping
+case class MultipleParameterMapping(mappingLoctions: List[String]) extends ParameterMapping
+
 /**
  * Represents the parameters of a function
  * @param values the datatypes of each of the parameters
  * @param mappings the fields where each parameter maps.
  */
-case class ParametersDefinition(values: List[String], mappings: List[String | List[String]]):
+case class ParametersDefinition(values: List[String], mappings: List[ParameterMapping]):
   require(values.length == mappings.length)
   
   def length: Int = values.length
