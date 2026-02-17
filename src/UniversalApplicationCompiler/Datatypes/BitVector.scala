64d3eab1e0f1b654321ca8b023b09a6909362c65
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2026-02-17 16:36:31 +0100

    Created BitVector datatype

---
+++ src/UniversalApplicationCompiler/Datatypes/BitVector.scala
@@ -0,0 +1,25 @@
+package UniversalApplicationCompiler.Datatypes
+
+final case class BitVector private(value: BigInt, width: Int):
+
+  def +(other: BitVector): BitVector =
+    require(width == other.width)
+    BitVector(value + other.value, width=width)
+
+  def -(other: BitVector): BitVector =
+    require(width == other.width)
+    BitVector(value - other.value, width=width)
+
+  def *(other: BitVector): BitVector =
+    require(width == other.width)
+    BitVector(value * other.value, width = width)
+
+  def /(other: BitVector): BitVector =
+    require(width == other.width)
+    BitVector(value / other.value, width = width)
+
+object BitVector:
+  def apply(value: BigInt, width: Int): BitVector =
+    require(width > 0)
+    val normalizedValue: BigInt = value & ((BigInt(1) << width) - 1)
+    new BitVector(normalizedValue, width)
