c9a45875fe49e889eaec60c855b4130ebcde5cc6
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-04-29 21:35:59 +0200

    Implemented parse and parseRecursively

--- src/UniversalApplicationCompiler/IsaParser.scala
+++ src/UniversalApplicationCompiler/IsaParser.scala
@@ -10,6 +10,8 @@ import UniversalApplicationCompiler.helpers.Conversions
  * @param autoParse if true, the parsing process starts automatically. If false, IsaParser.parse() must be called.
  */
 class IsaParser(val yamlConfigPath: String, var autoParse: Boolean = true):
+
+  //loads the YAML data and converts it to Scala datatypes
   val yamlData: Any =
     val yaml = new Yaml()
     val raw = yaml.load(Files.newInputStream(Paths.get(yamlConfigPath)))
@@ -18,11 +20,23 @@ class IsaParser(val yamlConfigPath: String, var autoParse: Boolean = true):
   //Auto-parse during construction if autoParse is enabled.
   if autoParse then parse()
 
-  def parse(): Unit = ???
+  def parse(): Unit =
+
+    yamlData match
+      case m: Map[?, ?] if m.keys.forall(_.isInstanceOf[String]) =>
+        val map = m.asInstanceOf[Map[String, Any]]
+        parseRecursively(map) //start the recursive parsing
+      case other => ??? //currently not implemented
 
   //Top-level parsers
-  private def parseRecursively(): Unit = ???
-  private def parseLevel(): Array[String] = ???
+  private def parseRecursively(currentLevel: Map[String, Any]): Unit =
+
+    val sublevels = parseLevel(currentLevel)
+
+    for sublevel <- sublevels do
+      parseRecursively(sublevel)
+
+  private def parseLevel(currentLevel: Map[String, Any]): Array[Map[String, Any]] = ???
 
   //parse structures
   private def parseInstruction(): Unit = ???
