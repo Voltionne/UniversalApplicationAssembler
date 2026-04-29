87b5c6065c8b3128d59e9cd4e9f7a4aa9f2df7d4
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-04-29 19:55:17 +0200

    Improved docstring

--- src/UniversalApplicationCompiler/helpers/ParametersDefinition.scala
+++ src/UniversalApplicationCompiler/helpers/ParametersDefinition.scala
@@ -5,11 +5,15 @@ case class SingleParameterMapping(mappingLocation: String) extends ParameterMapp
 case class MultipleParameterMapping(mappingLoctions: List[String]) extends ParameterMapping
 
 /**
- * Represents the parameters of a function
+ * Represents the parameters of an instruction
  * @param values the datatypes of each of the parameters
  * @param mappings the fields where each parameter maps.
  */
 case class ParametersDefinition(values: List[String], mappings: List[ParameterMapping]):
   require(values.length == mappings.length)
-  
+
+  /**
+   * Returns how many parameters
+   * @return length
+   */
   def length: Int = values.length
