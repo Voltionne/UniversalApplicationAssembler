7c11d462956a58fde078bbba5eb449bd00127815
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-04-29 19:59:16 +0200

    Changed "values" to "datatypes" for clarity.
    THIS WILL AFFECT YAML FILE DECLARATION

--- src/UniversalApplicationCompiler/helpers/ParametersDefinition.scala
+++ src/UniversalApplicationCompiler/helpers/ParametersDefinition.scala
@@ -2,18 +2,18 @@ package UniversalApplicationCompiler.helpers
 
 sealed trait ParameterMapping
 case class SingleParameterMapping(mappingLocation: String) extends ParameterMapping
-case class MultipleParameterMapping(mappingLoctions: List[String]) extends ParameterMapping
+case class MultipleParameterMapping(mappingLocations: List[String]) extends ParameterMapping
 
 /**
  * Represents the parameters of an instruction
- * @param values the datatypes of each of the parameters
+ * @param datatypes the datatypes of each of the parameters
  * @param mappings the fields where each parameter maps.
  */
-case class ParametersDefinition(values: List[String], mappings: List[ParameterMapping]):
-  require(values.length == mappings.length)
+case class ParametersDefinition(datatypes: List[String], mappings: List[ParameterMapping]):
+  require(datatypes.length == mappings.length)
 
   /**
    * Returns how many parameters
    * @return length
    */
-  def length: Int = values.length
+  def length: Int = datatypes.length
