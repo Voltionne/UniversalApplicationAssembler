f5d9692ffba4a25c70351f2ef1d583dabe84c9a6
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-04-30 12:04:18 +0200

    Returned to the classical organization

--- src/UniversalApplicationCompiler/helpers/TranslationContext.scala
+++ src/UniversalApplicationCompiler/helpers/TranslationContext.scala
@@ -1,17 +1,12 @@
 package UniversalApplicationCompiler.helpers
 
 /**
- * Represents the top translation context, from here everything should be leaves.
- */
-case class TranslationContext():
-  val children: scala.collection.mutable.Map[String, TranslationContextLeaf] = scala.collection.mutable.Map() //start with a map that is empty
-
-/**
  * Represents a single translation scope that is inseparable (i.e. atomic), which can be either a BitRange or a translation table. For allowing recursive scopes, there is the children dictionary.
  * @param leaf The BitRange or translation table that sits on this scope.
  */
-case class TranslationContextLeaf(leaf: BitRange | Map[String, BigInt]):
-  val children: scala.collection.mutable.Map[String, TranslationContextLeaf] = scala.collection.mutable.Map() //start with a map that is empty
+//For representing the top translation context, simply set some random BitRange that will never be called, as search() will always search between the childs.
+case class TranslationContext(leaf: BitRange | Map[String, BigInt]):
+  val children: scala.collection.mutable.Map[String, TranslationContext] = scala.collection.mutable.Map() //start with a map that is empty
 
   /**
    * Searches recursively through all children to return a certain nested TranslationContext
@@ -19,10 +14,10 @@ case class TranslationContextLeaf(leaf: BitRange | Map[String, BigInt]):
    * @param referenceString the string that indicates the wanted TranslationContext, each level separated with a point.
    * @return The wanted TranslationContext
    */
-  def search(referenceString: String): TranslationContextLeaf =
+  def search(referenceString: String): TranslationContext =
     val references = referenceString.split(".")
 
-    var currentTranslationContext: TranslationContextLeaf = this
+    var currentTranslationContext: TranslationContext = this
 
     for reference <- references do
       currentTranslationContext = currentTranslationContext.children(reference)
