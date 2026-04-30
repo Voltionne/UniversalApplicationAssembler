97c8991301687c7708048190cb590c500b2830cc
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-04-30 15:09:32 +0200

    Reorganized as 2-pass parsing.
    Finished also parseRecursively

--- src/UniversalApplicationCompiler/IsaParser.scala
+++ src/UniversalApplicationCompiler/IsaParser.scala
@@ -1,8 +1,9 @@
 package UniversalApplicationCompiler
 
 import org.yaml.snakeyaml.Yaml
+
 import java.nio.file.{Files, Paths}
-import UniversalApplicationCompiler.helpers.Conversions
+import UniversalApplicationCompiler.helpers.{BitRange, Conversions, TranslationContext, Node, Leaf}
 
 /**
  * Interprets an ISA based on a YAML file.
@@ -16,28 +17,56 @@ class IsaParser(val yamlConfigPath: String, var autoParse: Boolean = true):
     val yaml = new Yaml()
     val raw = yaml.load(Files.newInputStream(Paths.get(yamlConfigPath)))
     Conversions.convertFromJava(raw)
+  var bits: Int = -1
 
   //Auto-parse during construction if autoParse is enabled.
-  if autoParse then parse()
-
+  if autoParse then
+    parse()
+    
   def parse(): Unit =
+    parseFirstPass()
+    parseSecondPass()
+    
+  //-----------------------------------------
+  // FIRST PASS
+  //-----------------------------------------  
+
+  private def parseFirstPass(): Unit =
+
+    //The top TranslationContext
+    val currentTranslationContext = Node() //Literally none, this is the top level.
 
     yamlData match
       case m: Map[?, ?] if m.keys.forall(_.isInstanceOf[String]) =>
         val map = m.asInstanceOf[Map[String, Any]]
-        parseRecursively(map) //start the recursive parsing
+        parseRecursively(map, currentTranslationContext) //start the recursive parsing
       case other =>
         throw new IllegalArgumentException("Expected Map[String, Any]")
-
+        
   //Top-level parsers
-  private def parseRecursively(currentLevel: Map[String, Any]): Unit =
+  private def parseRecursively(currentLevel: Map[String, Any], currentTranslationContext: Node): Unit =
 
-    val sublevels = parseLevel(currentLevel)
+    val sublevels = parseLevel(currentLevel, currentTranslationContext)
 
     for sublevel <- sublevels do
-      parseRecursively(sublevel)
+      
+      currentLevel(sublevel) match
+        case m: Map[?, ?] if m.keys.forall(_.isInstanceOf[String]) =>
+          
+          val newTranslationContext = Node()
+          currentTranslationContext.addChild(newTranslationContext, sublevel)
+      
+          parseRecursively(m.asInstanceOf[Map[String, Any]], newTranslationContext)
 
-  private def parseLevel(currentLevel: Map[String, Any]): Array[Map[String, Any]] = ???
+        case other => throw new IllegalArgumentException("Expected Map[String, Any]")
+
+  private def parseLevel(currentLevel: Map[String, Any], currentTranslationContext: Node): Array[String] = ???
+
+  //-----------------------------------------
+  // SECOND PASS
+  //-----------------------------------------
+        
+  private def parseSecondPass(): Unit = ???
 
   //parse structures
   private def parseInstruction(): Unit = ???
