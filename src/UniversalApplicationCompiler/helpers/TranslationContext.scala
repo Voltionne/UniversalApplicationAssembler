eab7118628ca56ce228532a34a1c0ed47c88eedc
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-04-30 14:31:07 +0200

    Separated in to Node and Leaf

--- src/UniversalApplicationCompiler/helpers/TranslationContext.scala
+++ src/UniversalApplicationCompiler/helpers/TranslationContext.scala
@@ -4,20 +4,18 @@ import scala.annotation.tailrec
 import scala.collection.mutable
 
 /**
- * Represents a single translation scope that is inseparable (i.e. atomic), which can be either a BitRange or a translation table. For allowing recursive scopes, there is the children dictionary.
- *
- * @param leaf The BitRange or translation table that sits on this scope.
+ * Groups all the classes used to create the TranslationContext tree
  */
-//For representing the top translation context, simply set some random BitRange that will never be called, as search() will always search between the childs.
-case class TranslationContext(leaf: BitRange | Map[String, BigInt]):
-  val children: mutable.Map[String, TranslationContext] = mutable.Map.empty //start with a map that is empty
-  var parent: Option[TranslationContext] = None
+sealed trait TranslationContext:
+  var parent: Option[Node] = None //Parent will always be a Node, as they are the only ones who can have children.
 
-  /**
-   * Adds a child to the TranslationContext
-   * @param child the child, also a TranslationContext
-   * @param childName The name of the child, used during searching
-   */
+/**
+ * Represents a node of the TranslationContext tree, it has children which can be other Nodes or Leaves.
+ */
+case class Node() extends TranslationContext:
+  //As it is not the final Node, it can have children
+  val children: mutable.Map[String, TranslationContext] = mutable.Map.empty
+
   def addChild(child: TranslationContext, childName: String): Unit =
     if children.values.exists(_ eq child) then
       throw new IllegalArgumentException(s"Duplicate child!: ${child}")
@@ -25,40 +23,36 @@ case class TranslationContext(leaf: BitRange | Map[String, BigInt]):
     if child.parent.exists(_ != this) then
       throw new IllegalArgumentException("Child has already a parent!")
 
-    children(childName) = child
-    child.parent = Some(this)
-
-  /**
-   * Searches recursively through all children to return a certain nested TranslationContext
-   *
-   * @param referenceString the string that indicates the wanted TranslationContext, each level separated with a point.
-   * @return The wanted TranslationContext
-   */
   def search(referenceString: String): TranslationContext =
     val references = referenceString.split(".")
 
-    var currentTranslationContext = this
+    var currentTranslationContext: TranslationContext = this
 
     for reference <- references do
-      currentTranslationContext = currentTranslationContext.children(reference)
+
+      currentTranslationContext match
+        case n: Node => currentTranslationContext = n.children(reference)
+        case other => throw new IllegalArgumentException("Expected a Node, not a Leaf!")
 
     currentTranslationContext
 
-  /**
-   * Gets a map with all children and parents that can be seen in the scope of this certain TranslationContext
-   * @return An immutable map with the leaves in this scope.
-   */
   def getScope: Map[String, TranslationContext] =
 
     var currentTranslationContext = this
 
     @tailrec
-    def recursiveCall(translationContext: Option[TranslationContext], current: Map[String, TranslationContext]): Map[String, TranslationContext] =
-      translationContext match
+    def recursiveCall(node: Option[Node], current: Map[String, TranslationContext]): Map[String, TranslationContext] =
+      node match
         case None => current
         case Some(parent) =>
           val updated = current ++ parent.children
 
-          recursiveCall(translationContext, updated)
+          recursiveCall(node, updated)
 
     recursiveCall(parent, children.toMap)
+
+/**
+ * Represents a Leaf of the TranslationContext tree, it has a parent but no children.
+ * @param leaf The leaf, which can be either a BitRange or a translation table (i.e. a map)
+ */
+case class Leaf(leaf: BitRange | Map[String, BigInt]) extends TranslationContext
