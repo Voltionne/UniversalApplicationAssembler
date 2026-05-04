a2ad31233f302ec48d68fc15acbf0b4802f0fd41
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-05-04 14:38:13 +0200

    Added docstring

--- core/src/main/scala/UniversalApplicationAssembler/parsing/yaml/YamlReader.scala
+++ core/src/main/scala/UniversalApplicationAssembler/parsing/yaml/YamlReader.scala
@@ -6,10 +6,18 @@ import org.snakeyaml.engine.v2.nodes.Node
 
 import java.io.InputStream
 
+/**
+ * Provides methods for reading a YAML file using SnakeYAML-engine
+ */
 object YamlReader:
 
   private val settings: LoadSettings = LoadSettings.builder().build() //Default arguments are nice enough
 
+  /**
+   * Reads a YAML file and returns it as a tree of nodes.
+   * @param yamlConfigInputStream The input stream of the file
+   * @return The top node of the tree
+   */
   def readYamlFile(yamlConfigInputStream: InputStream): Node =
     val compose = new Compose(settings)
     compose.composeInputStream(yamlConfigInputStream).orElseThrow()
