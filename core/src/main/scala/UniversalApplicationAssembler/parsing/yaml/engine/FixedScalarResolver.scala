84a363306bd5b4f5b866c176170279d1683b7c2e
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-05-06 09:57:47 +0200

    Fixed initialization bug

--- core/src/main/scala/UniversalApplicationAssembler/parsing/yaml/engine/FixedScalarResolver.scala
+++ core/src/main/scala/UniversalApplicationAssembler/parsing/yaml/engine/FixedScalarResolver.scala
@@ -7,12 +7,13 @@ import java.util.regex.Pattern
 
 class FixedScalarResolver(supportMerge: Boolean) extends CoreScalarResolver(supportMerge):
 
-  private val BINARY_PATTERN = Pattern.compile("^[-+]?0b[0-1_]+$")
+  private lazy val BINARY_PATTERN = Pattern.compile("^[-+]?0b[0-1_]+$")
 
-  private val HEX_PATTERN = Pattern.compile("^[-+]?0x[0-9a-fA-F_]+$")
+  private lazy val HEX_PATTERN = Pattern.compile("^[-+]?0x[0-9a-fA-F_]+$")
 
   override def addImplicitResolvers(): Unit =
-    super.addImplicitResolvers()
 
     addImplicitResolver(Tag.INT, BINARY_PATTERN, "-+0")
     addImplicitResolver(Tag.INT, HEX_PATTERN, "-+0")
+
+    super.addImplicitResolvers()
