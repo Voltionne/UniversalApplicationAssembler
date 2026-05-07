35673906ec98bd53db3e72587ba48c4df3b4bf5d
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-05-07 15:04:50 +0200

    Added bitsToBytes

--- core/src/main/scala/UniversalApplicationAssembler/internal/parsing/assembly/AssemblyParser.scala
+++ core/src/main/scala/UniversalApplicationAssembler/internal/parsing/assembly/AssemblyParser.scala
@@ -9,7 +9,7 @@ object AssemblyParser:
    * @return Each instruction separated into opcode and operands.
    */
   def parseToList(assemblyFile: String): List[Array[String]] =
-  
+
     //Instruction format: (May change in the future)
     //[OPERAND] [PARAM1], [PARAM2], [PARAM3] [...]
 
@@ -28,3 +28,22 @@ object AssemblyParser:
 
     //Prepends everything for performance, now reverse. This is O(n)
     temp.reverse
+
+  /**
+   * Converts a string representing a sequence of 0s and 1s to an array of bytes.
+   * @param bits The string representing the single bits
+   * @return An array of bytes
+   */
+  def bitsToBytes(bits: String): Array[Byte] =
+
+    //Right-padding for multiple of 8, for bytes.
+    val padded =
+      val mod = bits.length % 8
+      if mod == 0 then bits
+      else "0" * (8 - mod) + bits
+
+    padded.grouped(8)
+      .map { byteStr =>
+        Integer.parseInt(byteStr, 2).toByte
+      }
+      .toArray
