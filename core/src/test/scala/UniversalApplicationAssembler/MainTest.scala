97c0ca7a8980f155e13838825f31ebdb3472c36a
SpaceProgrammer <spaceprogrammerbusiness@gmail.com>
2026-08-12 17:04:46 +0200

    Changed tests to use input streams (to fix a bug)

--- core/src/test/scala/UniversalApplicationAssembler/MainTest.scala
+++ core/src/test/scala/UniversalApplicationAssembler/MainTest.scala
@@ -3,45 +3,37 @@ package UniversalApplicationAssembler
 import UniversalApplicationAssembler.api.parsing.assembly.CustomAssembler
 import UniversalApplicationAssembler.api.parsing.isa.IsaParser
 
-import java.nio.file.{Files, Path}
-
-object Helper:
-
-  def getResourcePath(name: String): Path =
-
-    val url = getClass.getResource(name)
-    java.nio.file.Paths.get(url.toURI)
-
+import java.nio.file.Files
 
 class MainTest extends munit.FunSuite:
 
   test("Full test") {
 
-    val path = Helper.getResourcePath("/testIsa2.yaml")
+    val stream = getClass.getResourceAsStream("/testIsa2.yaml")
 
-    val (instructionMapping, node) = IsaParser.debugParse(path)
+    val (instructionMapping, node) = IsaParser.debugParse(stream)
 
     println("Tree:")
     visualizeNodes(node)
 
     val customAssembler = CustomAssembler(instructionMapping)
 
-    val inputPath = Helper.getResourcePath("/assembly.asm")
+    val inputStream = getClass.getResourceAsStream("/assembly.asm")
 
     val outputDir = Files.createTempDirectory("uaa-results-test1")
     println(s"Temp path: $outputDir")
     val outputPathString = outputDir.resolve("test-string.text")
     val outputPathBinary = outputDir.resolve("test-binary.txt")
 
-    customAssembler.compileToString(inputPath, outputPathString)
-    customAssembler.compileToBinary(inputPath, outputPathBinary)
+    customAssembler.compileToString(inputStream, outputPathString)
+    customAssembler.compileToBinary(inputStream, outputPathBinary)
   }
 
   test("Test limits yaml") {
 
-    val path = Helper.getResourcePath("/testIsa3.yaml")
+    val stream = getClass.getResourceAsStream("/testIsa3.yaml")
 
-    val (instructionMapping, node) = IsaParser.debugParse(path)
+    val (instructionMapping, node) = IsaParser.debugParse(stream)
 
     println("Tree:")
     visualizeNodes(node)
