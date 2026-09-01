5dcb487698d42cc5eb4be89f531fbf52672d37d1
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-09-01 19:56:57 +0200

    Replaced FullPath with Path (which is cleaner)

---
+++ core/src/main/scala/UniversalApplicationAssembler/internal/datatypes/Path.scala
@@ -0,0 +1,60 @@
+package UniversalApplicationAssembler.internal.datatypes
+
+import UniversalApplicationAssembler.internal.parsing.yaml.translation.TranslationNode
+
+case class Path(identifiers: List[String], base: List[String]):
+
+  /**
+   * Checks if the path is absolute
+   * @return True if absolute
+   */
+  def isAbsolute: Boolean = base.isEmpty
+
+  /**
+   * Returns the path as absolute
+   * @return The path as absolute
+   */
+  def getAbsolute: Path =
+    Path(base ++ identifiers, List.empty)
+
+  /**
+   * Returns the path but one scope lower
+   * @return The path one scope lower
+   */
+  def moveDown: Path =
+    Path(identifiers.tail, base :+ identifiers.head)
+
+  def +(other: Path): Path =
+    require(base == other.base, s"Trying to add two paths with different bases!: Path 1: $this Path 2: $other")
+
+    Path(identifiers ++ other.identifiers, base)
+
+  def :+(other: String): Path =
+    Path(identifiers :+ other, base)
+
+object Path:
+  def apply(stringPath: String, currentTranslationContext: TranslationNode): Path =
+
+    /*
+    2 cases:
+    1. The path is absolute
+    2. The path is local
+     */
+
+    if stringPath contains '.' then //100% absolute
+
+      require(stringPath.head != '.' && stringPath.last != '.', s"Absolute path \"$stringPath\" contains trailing dots!")
+
+      val scopes = stringPath.split('.')
+
+      Path(scopes.toList, List.empty)
+    else //100% local
+
+      Path(List(stringPath), currentTranslationContext.getPath.identifiers)
+
+  /**
+   * Returns an empty path
+   * @return An empty path
+   */
+  def empty: Path =
+    Path(List.empty, List.empty)
