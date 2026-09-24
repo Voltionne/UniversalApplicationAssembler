2cd704053af91dc9c273a8462c5a0869f825d7fe
SpaceProgrammerOriginal <spaceprogrammerbusiness@gmail.com>
2026-09-24 15:02:12 +0200

    Renamed tests and added new one

---
+++ core/src/test/resources/Test1_1.asm
@@ -0,0 +1,9 @@
+//This is a clean test
+
+WUPP r0, 0
+WLOW r0, 1
+ADD r0, r0, r1
+ADD r0, 10, r2
+WAIT
+
+//It should generate 5 instructions
--- core/src/test/scala/UniversalApplicationAssembler/MainTest.scala
+++ core/src/test/scala/UniversalApplicationAssembler/MainTest.scala
@@ -28,15 +28,22 @@ class MainTest extends munit.FunSuite:
 
     val customAssembler = CustomAssembler(instructionMapping)
 
-    val inputStream = getClass.getResourceAsStream("/Test1.asm")
-
     val outputDir = Files.createTempDirectory("uaa-test1-results")
     println(s"Temp path Test1: $outputDir")
-    val outputPathString = outputDir.resolve("test-string.txt")
-    val outputPathBinary = outputDir.resolve("test-binary.txt")
 
-    customAssembler.compileToString(inputStream, outputPathString)
-    customAssembler.compileToBinary(inputStream, outputPathBinary)
+    val inputStream0 = getClass.getResourceAsStream("/Test1_0.asm")
+    val outputPathString0 = outputDir.resolve("Test1_0-string.txt")
+    val outputPathBinary0 = outputDir.resolve("Test1_0-binary.txt")
+
+    val inputStream1 = getClass.getResourceAsStream("/Test1_1.asm")
+    val outputPathString1 = outputDir.resolve("Test1_1-string.txt")
+    val outputPathBinary1 = outputDir.resolve("Test1_1-binary.txt")
+
+    customAssembler.compileToString(inputStream0, outputPathString0)
+    customAssembler.compileToBinary(inputStream0, outputPathBinary0)
+
+    customAssembler.compileToString(inputStream1, outputPathString1)
+    customAssembler.compileToBinary(inputStream1, outputPathBinary1)
   }
 
   test("Test2") {
@@ -54,15 +61,14 @@ class MainTest extends munit.FunSuite:
 
     val customAssembler = CustomAssembler(instructionMapping)
 
-    val inputStream = getClass.getResourceAsStream("/Test2.asm")
-
     val outputDir = Files.createTempDirectory("uaa-test2-results")
     println(s"Temp path Test2: $outputDir")
 
-    val outputPathString = outputDir.resolve("test-string.txt")
-    val outputPathBinary = outputDir.resolve("test-binary.txt")
+    val inputStream0 = getClass.getResourceAsStream("/Test2_0.asm")
+    val outputPathString0 = outputDir.resolve("Test2_0-string.txt")
+    val outputPathBinary0 = outputDir.resolve("Test2_0-binary.txt")
 
-    customAssembler.compileToString(inputStream, outputPathString)
-    customAssembler.compileToBinary(inputStream, outputPathBinary)
+    customAssembler.compileToString(inputStream0, outputPathString0)
+    customAssembler.compileToBinary(inputStream0, outputPathBinary0)
 
   }
